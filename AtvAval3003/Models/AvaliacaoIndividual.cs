8c7863021bbbc192b3eac5740bde7f90f3596796
MatheusMottaGit <matheusprofess@gmail.com>
2026-03-30 19:42:34 -0300

    completando classes

---
+++ AtvAval3003/Models/AvaliacaoIndividual.cs
@@ -0,0 +1,15 @@
+﻿namespace AtvAval3003.Models
+{
+    public class AvaliacaoIndividual
+    {
+        public int Id { get; set; }
+
+        public float Nota { get; set; }
+
+        public int AvaliadorId { get; set; }
+        public Avaliador Avaliador { get; set; }
+
+        public int AlunoId { get; set; }
+        public Aluno Aluno { get; set; }
+    }
+}
---
+++ AtvAval3003/Models/Avaliador.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AtvAval3003.Models
+{
+    public class Avaliador
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Nome { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Telefone { get; set; }
+
+        public ICollection<AvaliacaoIndividual> AvaliacoesIndividuais { get; set; }
+
+        public Avaliador()
+        {
+            AvaliacoesIndividuais = new List<AvaliacaoIndividual>();
+        }
+
+        public Avaliador(string nome, string email, string telefone)
+        {
+            Nome = nome;
+            Email = email;
+            Telefone = telefone;
+            AvaliacoesIndividuais = new List<AvaliacaoIndividual>();
+        }
+    }
+}
